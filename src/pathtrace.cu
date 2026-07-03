971c84861955b519720238d813e410ff910ddb86
charlesAcmen <2021117312@stumail.nwu.edu.cn>
2026-07-03 16:16:25 +0800

    preserve no stochastic sampled antialiasing

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -153,7 +153,11 @@ __global__ void generateRayFromCamera(Camera cam, int iter, int traceDepth, Path
         segment.color = glm::vec3(1.0f, 1.0f, 1.0f);
 
         // TODO: implement antialiasing by jittering the ray
-        // Antialiasing: Add random jitter to ray direction for stochastic sampling
+        // segment.ray.direction = glm::normalize(cam.view
+        //     - cam.right * cam.pixelLength.x * ((float)x  - (float)cam.resolution.x * 0.5f)
+        //     - cam.up * cam.pixelLength.y * ((float)y - (float)cam.resolution.y * 0.5f)
+        // );
+        // // Antialiasing: Add random jitter to ray direction for stochastic sampling
         thrust::default_random_engine rng = makeSeededRandomEngine(iter, index, 0);
         thrust::uniform_real_distribution<float> u01(0, 1);
         float jitterX = u01(rng) - 0.5f;  // Random offset in [-0.5, 0.5]
