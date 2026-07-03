fb2c72633cbf62d4c10d647128eddc8744f69ad2
charlesAcmen <2021117312@stumail.nwu.edu.cn>
2026-07-03 16:03:38 +0800

    lens effect jitter ray

--- src/pathtrace.cu
+++ src/pathtrace.cu
@@ -153,9 +153,15 @@ __global__ void generateRayFromCamera(Camera cam, int iter, int traceDepth, Path
         segment.color = glm::vec3(1.0f, 1.0f, 1.0f);
 
         // TODO: implement antialiasing by jittering the ray
+        // Antialiasing: Add random jitter to ray direction for stochastic sampling
+        thrust::default_random_engine rng = makeSeededRandomEngine(iter, index, 0);
+        thrust::uniform_real_distribution<float> u01(0, 1);
+        float jitterX = u01(rng) - 0.5f;  // Random offset in [-0.5, 0.5]
+        float jitterY = u01(rng) - 0.5f;
+        
         segment.ray.direction = glm::normalize(cam.view
-            - cam.right * cam.pixelLength.x * ((float)x - (float)cam.resolution.x * 0.5f)
-            - cam.up * cam.pixelLength.y * ((float)y - (float)cam.resolution.y * 0.5f)
+            - cam.right * cam.pixelLength.x * ((float)x + jitterX - (float)cam.resolution.x * 0.5f)
+            - cam.up * cam.pixelLength.y * ((float)y + jitterY - (float)cam.resolution.y * 0.5f)
         );
 
         segment.pixelIndex = index;
