41c4540b4c91faa0be4abe391807fc1a29034a98
charlesAcmen <15591542871@163.com>
2026-07-23 15:46:48 +0800

    chore:tidy up ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -142,26 +142,7 @@ void RenderImGui()
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    static float f = 0.0f;
-    static int counter = 0;
-
-    ImGui::Begin("Path Tracer Analytics");                  // Create a window called "Hello, world!" and append into it.
-
-    // LOOK: Un-Comment to check the output window and usage
-    //ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-    //ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-    //ImGui::Checkbox("Another Window", &show_another_window);
-
-    //ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-    //ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-    //if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-    //    counter++;
-    //ImGui::SameLine();
-    //ImGui::Text("counter = %d", counter);
+    ImGui::Begin("Path Tracer Analytics");
 
     ImGui::Text("Traced Depth %d", imguiData->TracedDepth);
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
@@ -261,6 +242,16 @@ void RenderImGui()
             float vigExponent = getVignetteExponent();
             if (ImGui::SliderFloat("Vignette Exponent", &vigExponent, 0.5f, 8.0f, "%.1f"))
                 setVignetteExponent(vigExponent);
+        }
+
+        ImGui::Separator();
+        {
+            SceneStats stats = computeSceneStats(*scene);
+            ImGui::Text("Scene: %d objects", stats.numObjects);
+            ImGui::Text("  meshes: %d  spheres: %d  cubes: %d",
+                        stats.numMeshes, stats.numSpheres, stats.numCubes);
+            ImGui::Text("  %d triangles, %d materials",
+                        stats.numTriangles, stats.numMaterials);
         }
     }
     ImGui::End();
