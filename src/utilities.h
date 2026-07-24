c48ce57b600b64ac56e0e85bc4d20eea7b7fec3c
charlesAcmen <15591542871@163.com>
2026-07-24 15:43:17 +0800

    remove unused starter code in utility core

--- src/utilities.h
+++ src/utilities.h
@@ -3,24 +3,12 @@
 #include "constants.h"
 #include "glm/glm.hpp"
 
-#include <algorithm>
-#include <istream>
-#include <iterator>
-#include <ostream>
-#include <sstream>
 #include <string>
-#include <vector>
 
 namespace utilityCore
 {
-    extern float clamp(float f, float min, float max);
-    extern bool replaceString(std::string& str, const std::string& from, const std::string& to);
-    extern glm::vec3 clampRGB(glm::vec3 color);
-    extern bool epsilonCheck(float a, float b);
-    extern std::vector<std::string> tokenizeString(std::string str);
-    extern glm::mat4 buildTransformationMatrix(glm::vec3 translation, glm::vec3 rotation, glm::vec3 scale);
     extern std::string convertIntToString(int number);
-    extern std::istream& safeGetline(std::istream& is, std::string& t); //Thanks to http://stackoverflow.com/a/6089413
+    extern glm::mat4 buildTransformationMatrix(glm::vec3 translation, glm::vec3 rotation, glm::vec3 scale);
 }
 
 // CUDA error checking utilities
