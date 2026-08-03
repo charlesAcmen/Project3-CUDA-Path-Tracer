0baea46fa3b2d581dafd7bb17d5947d5f92e7b0e
charlesAcmen <2021117312@stumail.nwu.edu.cn>
2026-08-03 15:50:02 +0800

    move scene to folder

--- src/scene/scene.cpp
+++ src/scene/scene.cpp
@@ -1,4 +1,4 @@
-#include "scene.h"
+#include "scene/scene.h"
 
 // ====================================================================
 // Scene Statistics
@@ -8,7 +8,15 @@ SceneStats computeSceneStats(const Scene& scene)
 {
     SceneStats s;
     s.numObjects   = (int)scene.geoms.size();
-    s.numMeshes    = (int)scene.geoms.size();
+    
+    // Count actual meshes (geometries with triangle data).
+    s.numMeshes = 0;
+    for (const auto& geom : scene.geoms) {
+        if (geom.meshTriangleCount > 0) {
+            s.numMeshes++;
+        }
+    }
+    
     s.numMaterials = (int)scene.materials.size();
     s.numTriangles = (int)scene.hostTriangles.size();
     return s;
