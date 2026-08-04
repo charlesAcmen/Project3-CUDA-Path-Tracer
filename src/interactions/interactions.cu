816419c3873f12bdb953e2344e6271efcd856d71
charlesAcmen <2021117312@stumail.nwu.edu.cn>
2026-08-04 18:13:14 +0800

    add SPECULAR_EXPONENT_ZERO_EPSILON macro for skipping expensive powf op in Phone lobe calculation

--- src/interactions/interactions.cu
+++ src/interactions/interactions.cu
@@ -62,7 +62,7 @@ __host__ __device__ glm::vec3 samplePhongSpecularDir(
 
     // Eq.7: cos(theta_s) = xi1^(1/(n+1))
     // Optimization: if exponent is 0.0f (i.e. maximum roughness, r=1.0f), we skip powf
-    float cosTheta = (exponent < EPSILON) ? xi1 : powf(xi1, invExponentPlusOne);
+    float cosTheta = (exponent < SPECULAR_EXPONENT_ZERO_EPSILON) ? xi1 : powf(xi1, invExponentPlusOne);
     float sinTheta = sqrtf(fmaxf(0.0f, 1.0f - cosTheta * cosTheta));
 
     // Eq.8: phi_s = 2*pi*xi2
